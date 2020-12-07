1778404ca538aace4b6b0ae710b967ec9d39bce7
Komal Dhamu <71710063+komalthecoder@users.noreply.github.com>
2020-12-07 19:38:55 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,14 @@
+class Drops{
+
+    constructor(x,y){
+
+        var options = {
+
+            restittution = 0,
+            friction = 0.1
+
+        }
+        
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas;
+
+function preload(){
+    
+}
+
+function setup(){
+canvas = createCanvas(500,700);
+    
+}
+
+function draw(){
+    background(0,0,0);
+}   
+
